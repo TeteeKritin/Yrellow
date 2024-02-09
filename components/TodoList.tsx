31312056782951f87836c4ece25403b7a18594c3
TeteeKritin <kritin.te@gmail.com>
2024-02-10 03:17:09 +0700

    Add Delete & Checkbox

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,55 +1,34 @@
-"use client";
-import React, { useState, useEffect } from "react";
-
-
-// Make a request for a user with a given ID
-// axios.get('https://dummyjson.com/todos')
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .finally(function () {
-//     // always executed
-//   });
-
-// User can add, modify, delete, check, uncheck, and reorder todo list item.
-// The number of completed items and total items are required to display in the app (example format: 3/10).
-// A todo list app must have ability to save user data on local machine so they can revisit their own todo list later.
-// This todo list app contains one magic button for user when they don’t know what to add in the todo list. They can click this button to generate todo list which needs to be fetched from this API endpoint https://dummyjson.com/todos
-// For the UI, you can design it by your own way. Be creative!
-// Bonus - There is one optional field for each todo list item which is an image (yeah. user can attach image into a todo list item).
+import { time } from "console";
+import { todo } from "node:test";
+import React, { useState } from "react";
 
 interface Todo {
   id: number;
-  todo: string;
+  text: string;
   completed: boolean;
   userId: number;
 }
 
 const TodoList: React.FC = () => {
-  const [newTodoText, setNewTodoText] = useState('');
+  const [newTodoText, setNewTodoText] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
   const [editTodoId, setEditTodoId] = useState<number | null>(null);
-  const [editTodoText, setEditTodoText] = useState<string>('');
+  const [editTodoText, setEditTodoText] = useState<string>("");
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewTodoText(e.target.value);
+  const handleInputChange = (text: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodoText(text.target.value);
   };
 
-  const handleAddTodo = () => {
-    if (newTodoText.trim() !== '') {
+  const handleTodoAdd = () => {
+    if (newTodoText !== "") {
       const newTodo: Todo = {
         id: Date.now(),
-        todo: newTodoText,
+        text: newTodoText,
         completed: false,
         userId: Date.now(),
       };
-      setTodos(prevTodos => [...prevTodos, newTodo]);
-      setNewTodoText('');
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+      setNewTodoText("");
     }
   };
 
@@ -58,55 +37,80 @@ const TodoList: React.FC = () => {
     setEditTodoText(text);
   };
 
-  const handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditTodoText(e.target.value);
+  const handleEditChange = (text: React.ChangeEvent<HTMLInputElement>) => {
+    setEditTodoText(text.target.value);
   };
 
-  const handleUpdateTodo = (id: number) => {
-    const updatedTodos = todos.map(todo =>
-      todo.id === id ? { ...todo, todo: editTodoText } : todo
+  const todoUpdater = (id: number) => {
+    const updatedTodo = todos.map((todo) =>
+      todo.id === id ? { ...todo, text: editTodoText } : todo
     );
-    setTodos(updatedTodos);
+    setTodos(updatedTodo);
     setEditTodoId(null);
   };
 
+  const handleDeleteTodo = (id: number) => {
+    const updatedTodo = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodo);
+  };
+
+  const handleCheckBox = (id: number) => {
+    const updatedTodo = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodo);
+  };
+
+  const completedTodoCount = todos.filter((todo) => todo.completed).length;
+
   return (
-    <div className="text-white">
+    <div>
       <input
-        className="text-black"
         type="text"
-        placeholder="Enter your todo"
         value={newTodoText}
         onChange={handleInputChange}
-      />
-      <button onClick={handleAddTodo}>Add</button>
-      <div>
-        <ul>
-          {todos.map(todo => (
-            <div key={todo.id} className="container-sm bg-slate-500">
-              <li>
-                <input type="checkbox" />
-                {editTodoId === todo.id ? (
-                  <>
-                    <input
+        placeholder="Input your todo"
+      ></input>
+      <button className="text-white" onClick={handleTodoAdd}>
+        Add
+      </button>
+      <h1 className="text-white">
+        {}{completedTodoCount} /{todos.length}
+      </h1>
+      <ul>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <li className="text-white">
+              {editTodoId === todo.id ? (
+                <>
+                  <input
                     className="text-black"
-                      type="text"
-                      value={editTodoText}
-                      onChange={handleEditChange}
-                    />
-                    <button onClick={() => handleUpdateTodo(todo.id)}>Save</button>
-                  </>
-                ) : (
-                  <>
-                    <span>{todo.todo}</span>
-                    <button onClick={() => handleEditClick(todo.id, todo.todo)}>Edit</button>
-                  </>
-                )}
-              </li>
-            </div>
-          ))}
-        </ul>
-      </div>
+                    type="text"
+                    value={editTodoText}
+                    onChange={handleEditChange}
+                  ></input>
+                  <button onClick={() => todoUpdater(todo.id)}>Save</button>
+                </>
+              ) : (
+                <>
+                  <span>{todo.text}</span>
+                  <button onClick={() => handleEditClick(todo.id, todo.text)}>
+                    Edit
+                  </button>
+                  <button onClick={() => handleDeleteTodo(todo.id)}>
+                    Delete
+                  </button>
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={() => handleCheckBox(todo.id)}
+                  ></input>
+                </>
+              )}
+            </li>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
